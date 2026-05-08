cad568f332af98eedac7b5b33f2d230c4a501379
Mike Oliphant <oliphant@nostatic.org>
2026-05-08 16:32:08 -0700

    Display html with next song button

--- MusicLib/SongPlayContext.cs
+++ MusicLib/SongPlayContext.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using MusicLib;
+using SharpCifs.Netbios;
 using SharpCifs.Util.Transport;
 using TagLib.Ape;
 
@@ -98,33 +99,41 @@ namespace MusicLib
                     HttpListenerContext context = httpListener.GetContext();
                     HttpListenerRequest request = context.Request;
 
-                    if (request.HttpMethod == "GET")
-                    {
-                        using HttpListenerResponse response = context.Response;
-                        string responseString = JsonSerializer.Serialize<SongData>(CurrentSong);
-
-                        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-
-                        response.ContentLength64 = buffer.Length;
-                        response.ContentType = "application/json";
-
-                        using Stream output = response.OutputStream;
-                        output.Write(buffer, 0, buffer.Length);
-                    }
-                    else if (request.HttpMethod == "POST")
+                    if (request.HttpMethod == "POST")
                     {
                         NextSong();
-
-                        using HttpListenerResponse response = context.Response;
-
-                        response.StatusCode = (int)HttpStatusCode.OK;
-                        response.OutputStream.Close();
                     }
+
+                    using HttpListenerResponse response = context.Response;
+
+                    string responseString = $"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple POST Form</title>
+</head>
+<body>
+    <h1>{CurrentSong.Title}</h1>
+    <h1>{CurrentSong.Artist}</h1>
+    <form action="/submit-form-endpoint" method="post">
+        <button type="submit">Next Song</button>
+    </form>
+</body>
+</html>
+""";
+                    byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                    response.ContentLength64 = buffer.Length;
+                    response.ContentType = "text/html";
+
+                    using Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
                 }
                 catch (Exception ex)
                 {
 
-                }
+                }                
             }
         }
     }
